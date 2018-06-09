6c4479a533cccb13e7138f405a09b7f0fac50f17
adailtonribeiro <adailton.ribeiro1@gmail.com>
2018-06-09 15:02:30 -0300

    implement android methods

--- src/android/FirebasePerformancePlugin.java
+++ src/android/FirebasePerformancePlugin.java
@@ -1,18 +1,122 @@
 package com.adailtonribeiro.cordova.firebase;
 
+import java.util.HashMap;
+
 import android.util.Log;
+
+import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import com.google.firebase.perf.FirebasePerformance;;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.firebase.perf.FirebasePerformance;
+import com.google.firebase.perf.metrics.Trace;
 
 public class FirebasePerformancePlugin extends CordovaPlugin {
-    private static final String TAG = "FirebasePerformancePlugin";
+  private static final String TAG = "FirebasePerfPlugin";
 
-    private FirebasePerformance firebasePerformance;
+  private FirebasePerformance firebasePerformance;
+  private HashMap<String, Trace> traces = new HashMap<String, Trace>();
 
-    @Override
-    protected void pluginInitialize() {
-        Log.d(TAG, "Starting Firebase Performance plugin");
+  @Override
+  protected void pluginInitialize() {
+    Log.d(TAG, "Starting Firebase Performance plugin");
 
-        this.firebasePerformance = FirebasePerformance.getInstance();
+    this.firebasePerformance = FirebasePerformance.getInstance();
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if ("startTrace".equals(action)) {
+      startTrace(callbackContext, args.getString(0));
+      return true;
+    } else if ("stopTrace".equals(action)) {
+      stopTrace(callbackContext, args.getString(0));
+      return true;
+    } else if ("incrementCounter".equals(action)) {
+      incrementCounter(callbackContext, args.getString(0), args.getString(1));
+      return true;
     }
+
+    return false;
+  }
+
+  private void startTrace(final CallbackContext callbackContext, final String name) {
+    final FirebasePerformancePlugin self = this;
+    cordova.getThreadPool().execute(new Runnable() {
+      public void run() {
+        try {
+
+          Trace myTrace = null;
+          if (self.traces.containsKey(name)) {
+            myTrace = self.traces.get(name);
+          }
+
+          if (myTrace == null) {
+            myTrace = self.firebasePerformance.newTrace(name);
+            myTrace.start();
+            self.traces.put(name, myTrace);
+          }
+
+          callbackContext.success();
+        } catch (Exception e) {
+          e.printStackTrace();
+          callbackContext.error(e.getMessage());
+        }
+      }
+    });
+  }
+
+  private void incrementCounter(final CallbackContext callbackContext, final String name, final String counterNamed) {
+    final FirebasePerformancePlugin self = this;
+    cordova.getThreadPool().execute(new Runnable() {
+      public void run() {
+        try {
+
+          Trace myTrace = null;
+          if (self.traces.containsKey(name)) {
+            myTrace = self.traces.get(name);
+          }
+
+          if (myTrace != null) {
+            myTrace.incrementCounter(counterNamed);
+            callbackContext.success();
+          } else {
+            callbackContext.error("Trace not found");
+          }
+
+        } catch (Exception e) {
+          e.printStackTrace();
+          callbackContext.error(e.getMessage());
+        }
+      }
+    });
+  }
+
+  private void stopTrace(final CallbackContext callbackContext, final String name) {
+    final FirebasePerformancePlugin self = this;
+    cordova.getThreadPool().execute(new Runnable() {
+      public void run() {
+        try {
+
+          Trace myTrace = null;
+          if (self.traces.containsKey(name)) {
+            myTrace = self.traces.get(name);
+          }
+
+          if (myTrace != null) {
+            myTrace.stop();
+            self.traces.remove(name);
+            callbackContext.success();
+          } else {
+            callbackContext.error("Trace not found");
+          }
+
+        } catch (Exception e) {
+          e.printStackTrace();
+          callbackContext.error(e.getMessage());
+        }
+      }
+    });
+  }
 }
--- www/FirebasePerformance.js
+++ www/FirebasePerformance.js
@@ -1,10 +1,23 @@
-var exec = require("cordova/exec");
-var PLUGIN_NAME = "FirebasePerformance";
-
-module.exports = {
-    logEvent: function(name, params) {
-        return new Promise(function(resolve, reject) {
-            exec(resolve, reject, PLUGIN_NAME, "logEvent", [name, params || {}]);
-        });
-    }
-};
+cordova.define("cordova-plugin-firebase-performance.FirebasePerformance", function(require, exports, module) {
+    var exec = require("cordova/exec");
+    var PLUGIN_NAME = "FirebasePerformance";
+    
+      module.exports = {
+          startTrace: function(name) {
+              return new Promise(function(resolve, reject) {
+                  exec(resolve, reject, PLUGIN_NAME, "startTrace", [name]);
+              });
+          },
+          incrementCounter: function(name, counterNamed) {
+              return new Promise(function(resolve, reject) {
+                  exec(resolve, reject, PLUGIN_NAME, "incrementCounter", [name, counterNamed]);
+              });
+          },
+          stopTrace: function(name) {
+              return new Promise(function(resolve, reject) {
+                  exec(resolve, reject, PLUGIN_NAME, "stopTrace", [name]);
+              });
+          },
+      };
+});
+    
